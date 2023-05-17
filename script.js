2709eb3657824ec7ceca21272eb574f928fcf499
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-17 22:54:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const output = document.querySelector('#output');
+
+const prom = () => {
+	return new Promise((resolve) => setTimeout(resolve('Hello, world!'), 1000));
+}
+
+const callIt = async() => {
+	const data = await prom();
+	output.innerText = data;
+}
+
+callIt();
